8aee2271cc33368bc02a4e8c3795c345e1868159
karamvirsingh98 <karamvir.singh98@gmail.com>
2021-11-16 05:03:19 -0800

    remove boolean from get most recent

--- src/queries/get_most_recent.rs
+++ src/queries/get_most_recent.rs
@@ -2,11 +2,8 @@
 //! 
 //! Retrieves the n most recent items from the collection, and returns a Vec of the type that the collection is assigned to.
 //! 
-//! If latest_first is set to true, the 0th index of the Vec will be the most recent item that was added to the collection, 
-//! and if set to false, the 0th index of the Vec will be the nth item that was added to the collection.
-//! 
-//! If n is greater than the number of documents in the collection, the first document added to the collection 
-//! will either be the 0th or the last index, depending on whether latest_first is set to true or false. 
+//! If n is greater than the number of documents in the collection, the 0th index of the returned Vec
+//! will be the first document in the collection ..
 //! 
 //! # Examples
 //! 
@@ -24,12 +21,8 @@
 //! 
 //!     let n = 10
 //!     //returns the 10 most recent docs in the collection, 
-//!     //with the latest doc at the 0th index of the Vec
-//!     let latest_first = collection.get_most_recent(n, true).await.unwrap();
-//!     
-//!     //returns the 10 most recent docs in the colelction, 
-//!     //with the latest doc at the last index of the Vec
-//!     let oldest_first = collection.get_most_recent(n, false).await.unwrap();
+//!     //in order of oldest to latest
+//!     let items = collection.get_most_recent(n, true).await.unwrap();
 //! 
 //! ```
 //! 
@@ -42,7 +35,7 @@ use serde::de::DeserializeOwned;
 use crate::Collection;
 
 impl<T: DeserializeOwned + Unpin + Send + Sync> Collection<T> {
-    pub async fn get_most_recent(&self, num_items: i64, latest_first: bool) -> Result<Vec<T>> {
+    pub async fn get_most_recent(&self, num_items: i64) -> Result<Vec<T>> {
         let find_options = FindOptions::builder()
             .sort(doc! { "_id": -1 })
             .limit(num_items)
@@ -52,7 +45,7 @@ impl<T: DeserializeOwned + Unpin + Send + Sync> Collection<T> {
         while let Some(item) = cursor.try_next().await? {
             items.push(item);
         }
-        if !latest_first {items.reverse()};
+        items.reverse();
         Ok(items)
     }
 }
