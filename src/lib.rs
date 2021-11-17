ec17d295bd7cb91944741d9d6035712112493287
mbecker20 <becker.maxh@gmail.com>
2021-11-17 14:01:51 -0800

    0.0.9 reexport object ID

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 mod queries;
 mod mungos;
 mod collection;
-pub use mongodb::{options::FindOptions, bson::doc};
+pub use mongodb::{options::FindOptions, bson::{doc, oid::ObjectId}};
 pub use crate::{mungos::Mungos, collection::Collection};
