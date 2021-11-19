2c0162377940452384efa21c34322d7c88deeab6
mbecker20 <becker.maxh@gmail.com>
2021-11-19 02:04:27 -0800

    move to new collection 0.0.10

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 mod queries;
 mod mungos;
 mod collection;
+mod move_to_new_collection;
 pub use mongodb::{options::FindOptions, bson::{doc, oid::ObjectId}};
 pub use crate::{mungos::Mungos, collection::Collection};
