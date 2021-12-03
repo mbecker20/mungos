47cb6930805817f9cb400dd6f4f8f41e4fb7a186
mbecker20 <becker.maxh@gmail.com>
2021-12-03 14:21:40 -0800

    re export Serialize and Deserialize

--- src/lib.rs
+++ src/lib.rs
@@ -3,4 +3,5 @@ mod mungos;
 mod collection;
 pub mod helpers;
 pub use mongodb::{options::FindOptions, bson::{doc, oid::ObjectId, Document, to_bson}, error::Result};
+pub use serde::{Serialize, Deserialize};
 pub use crate::{mungos::Mungos, collection::Collection, queries::update_one::Update};
