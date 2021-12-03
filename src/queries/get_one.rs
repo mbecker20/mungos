527b73d3fc5b88cb269e2bf10239141ea72dbf96
mbecker20 <becker.maxh@gmail.com>
2021-12-03 01:46:26 -0800

    no double unwrap on get_one

--- src/queries/get_one.rs
+++ src/queries/get_one.rs
@@ -32,11 +32,12 @@ use std::str::FromStr;
 use crate::Collection;
 
 impl<T: DeserializeOwned + Unpin + Send + Sync> Collection<T> {
-    pub async fn get_one(&self, id: &str) -> Result<Option<T>> {
+    pub async fn get_one(&self, id: &str) -> Result<T> {
         let item = self
             .collection
             .find_one(Some(doc! { "_id": ObjectId::from_str(id).unwrap() }), None)
-            .await?;
+            .await?
+            .unwrap();
         Ok(item)
     }
 }
