b551609b72c1e01852c69c8833ab85c42177e6c4
beckerinj <becker@assc.ai>
2022-07-12 21:16:52 -0400

    0.2.10 reexport mongodb::error::Error

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@ mod queries;
 pub use crate::{collection::Collection, mungos::Mungos, queries::update_one::Update};
 pub use mongodb::{
     bson::{doc, oid::ObjectId, to_bson, Document},
-    error::Result,
+    error::{Result, Error},
     options::{FindOptions, Compressor},
     IndexModel,
 };
