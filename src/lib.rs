1e2e23b7a6ad927bdd941f96348338496626e23e
beckerinj <becker@assc.ai>
2022-07-13 19:55:29 -0400

    0.2.11 reexport all mongodb

--- src/lib.rs
+++ src/lib.rs
@@ -4,9 +4,10 @@ mod mungos;
 mod queries;
 pub use crate::{collection::Collection, mungos::Mungos, queries::update_one::Update};
 pub use mongodb::{
-    bson::{doc, oid::ObjectId, to_bson, Document},
-    error::{Result, Error},
-    options::{FindOptions, Compressor},
+    self,
+    bson::{self, doc, oid::ObjectId, to_bson, Document},
+    error::{self, Result, Error},
+    options::{self, FindOptions, Compressor},
     IndexModel,
 };
 pub use serde::{Deserialize, Serialize};
