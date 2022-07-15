99e566167d3774b41e7ae372c582dc203d680cdd
beckerinj <becker@assc.ai>
2022-07-14 23:05:11 -0400

    0.2.12 create index helpers

--- src/collection.rs
+++ src/collection.rs
@@ -1,5 +1,23 @@
-use mongodb::Collection as MongoCollection;
+use mongodb::{Collection as MongoCollection, IndexModel, bson::doc, error::Result, results::CreateIndexResult, options::IndexOptions};
 
 pub struct Collection<T> {
     pub collection: MongoCollection<T>,
+}
+
+impl<T> Collection<T> {
+    pub async fn create_index(&self, field: &str) -> Result<CreateIndexResult> {
+        let index = IndexModel::builder()
+            .keys(doc! { field: 1 })
+            .build();
+        self.collection.create_index(index, None).await
+    }
+
+    pub async fn create_unique_index(&self, field: &str) -> Result<CreateIndexResult> {
+        let options = IndexOptions::builder().unique(true).build();
+        let index = IndexModel::builder()
+            .keys(doc! { field: 1 })
+            .options(options)
+            .build();
+        self.collection.create_index(index, None).await
+    }
 }
