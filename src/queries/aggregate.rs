c6f604c9d39eb63b7d879fa652eabeab90cf3c41
beckerinj <becker@assc.ai>
2022-08-03 13:43:07 -0400

    aggregate collect

--- src/queries/aggregate.rs
+++ src/queries/aggregate.rs
@@ -1,5 +1,6 @@
 use mongodb::{bson::Document, error::Result, options::AggregateOptions, Cursor};
 use serde::de::DeserializeOwned;
+use futures::TryStreamExt;
 
 use crate::Collection;
 
@@ -10,5 +11,13 @@ impl<T: DeserializeOwned + Unpin + Send + Sync> Collection<T> {
         options: impl Into<Option<AggregateOptions>>,
     ) -> Result<Cursor<Document>> {
         self.collection.aggregate(pipeline, options).await
+    }
+
+    pub async fn aggregate_collect(
+        &self,
+        pipeline: impl IntoIterator<Item = Document>,
+        options: impl Into<Option<AggregateOptions>>,
+    ) -> Result<Vec<Document>> {
+        self.aggregate(pipeline, options).await?.try_collect().await
     }
 }
