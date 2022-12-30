640d3a83d488682344fe866411a9c67608e66f5f
beckerinj <becker@anticapital.ai>
2022-12-30 12:45:59 -0500

    0.2.27 pass refs to create queries

--- src/queries/create_many.rs
+++ src/queries/create_many.rs
@@ -22,13 +22,15 @@
 //! ```
 //!
 
+use std::borrow::Borrow;
+
 use mongodb::error::Result;
 use serde::Serialize;
 
 use crate::Collection;
 
 impl<T: Serialize> Collection<T> {
-    pub async fn create_many(&self, items: Vec<T>) -> Result<()> {
+    pub async fn create_many(&self, items: impl IntoIterator<Item = impl Borrow<T>>) -> Result<()> {
         self.collection.insert_many(items, None).await?;
         Ok(())
     }
--- src/queries/create_one.rs
+++ src/queries/create_one.rs
@@ -22,13 +22,15 @@
 //! ```
 //!
 
+use std::borrow::Borrow;
+
 use mongodb::error::Result;
 use serde::Serialize;
 
 use crate::Collection;
 
 impl<T: Serialize> Collection<T> {
-    pub async fn create_one(&self, item: T) -> Result<String> {
+    pub async fn create_one(&self, item: impl Borrow<T>) -> Result<String> {
         let res = self.collection.insert_one(item, None).await?;
         Ok(res.inserted_id.as_object_id().unwrap().to_string())
     }
