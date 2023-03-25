e8250fe9d91261bce3d9bd41e394daf880a9ad61
mbecker20 <becker.maxh@gmail.com>
2023-03-25 06:03:57 +0000

    bulk update/sert retry when bson too big

--- src/queries/bulk_update.rs
+++ src/queries/bulk_update.rs
@@ -1,8 +1,9 @@
 use std::borrow::Borrow;
 
+use anyhow::Context;
 use mongodb::{
     bson::{doc, Document},
-    error::Result,
+    error::{ErrorKind, Result},
 };
 
 use crate::Collection;
@@ -36,5 +37,52 @@ impl<T> Collection<T> {
             "updates": update_docs,
         };
         self.db.run_command(command, None).await
+    }
+
+    pub async fn bulk_update_retry_too_big(
+        &self,
+        updates: impl Borrow<Vec<BulkUpdate>>,
+    ) -> anyhow::Result<Document> {
+        let update_docs: Vec<Document> = updates
+            .borrow()
+            .into_iter()
+            .map(|update| {
+                doc! {
+                    "q": &update.query,
+                    "u": &update.update,
+                }
+            })
+            .collect();
+        let command = doc! {
+            "update": self.collection.name(),
+            "updates": &update_docs,
+        };
+        let res = self.db.run_command(command, None).await;
+        if res.is_ok() {
+            return res.context("");
+        }
+        let error_kind = &*res.as_ref().err().unwrap().kind;
+        if let ErrorKind::Command(c) = error_kind {
+            if c.code_name == "BSONObjectTooLarge" && update_docs.len() > 2 {
+                let (u1, u2) = update_docs.split_at(update_docs.len() / 2);
+                let c1 = doc! {
+                    "update": self.collection.name(),
+                    "updates": u1,
+                };
+                let c2 = doc! {
+                    "update": self.collection.name(),
+                    "updates": u2,
+                };
+                let (res1, res2) =
+                    tokio::join!(self.db.run_command(c1, None), self.db.run_command(c2, None));
+                let doc1 = res1?;
+                let doc2 = res2?;
+                return Ok(doc! {
+                    "res1": doc1,
+                    "res2": doc2
+                });
+            }
+        }
+        res.context("failed to bulk update")
     }
 }
