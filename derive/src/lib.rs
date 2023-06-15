8fa23299201bce4b7ca6842c24a547eaf20a04c0
mbecker20 <becker.maxh@gmail.com>
2023-06-15 02:06:19 +0000

    0.3.22 restructure indexing macro

--- derive/src/lib.rs
+++ derive/src/lib.rs
@@ -1,14 +1,14 @@
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, Data, DeriveInput, Expr};
+use syn::{parse_macro_input, Data, DeriveInput, Expr, Field};
 
 #[proc_macro_derive(
-    MungosColl,
+    MungosIndexed,
     attributes(index, unique_index, doc_index, unique_doc_index)
 )]
-pub fn derive_collection(input: TokenStream) -> TokenStream {
+pub fn derive_indexed(input: TokenStream) -> TokenStream {
     let DeriveInput {
-        ident, attrs, data, ..
+        ident, data, attrs, ..
     } = parse_macro_input!(input as DeriveInput);
 
     let mut doc_indexes = Vec::new();
@@ -27,42 +27,96 @@ pub fn derive_collection(input: TokenStream) -> TokenStream {
 
     let s = match data {
         Data::Struct(s) => s,
-        _ => panic!("must derive on struct"),
+        Data::Enum(_) => {
+            return quote! {
+                impl mungos::Indexed for #ident {}
+            }.into()
+        }
+        _ => panic!("must derive on struct or enum"),
     };
 
-    let mut unique_indexes = Vec::new();
     let mut indexes = Vec::new();
+    let mut unique_indexes = Vec::new();
 
-    for field in s.fields {
-        let is_unique = field
-            .attrs
+    for Field {
+        attrs, ident, ty, ..
+    } in s.fields
+    {
+        if ident.is_none() {
+            continue;
+        }
+        let ident = ident.unwrap();
+        let is_unique = attrs
             .iter()
             .any(|attr| attr.path().is_ident("unique_index"));
         if is_unique {
-            unique_indexes.push(field.ident.expect("field must have ident"));
+            unique_indexes.push(quote! {
+                unique_indexes.push(stringify!(#ident).to_string());
+            });
             continue;
         }
-        let is_index = field.attrs.iter().any(|attr| attr.path().is_ident("index"));
+        let is_index = attrs.iter().any(|attr| attr.path().is_ident("index"));
         if is_index {
-            indexes.push(field.ident.expect("field must have ident"));
+            indexes.push(quote! {
+                indexes.push(stringify!(#ident).to_string());
+            });
+            continue;
         }
+        indexes.push(quote! {
+            let nested = <#ty as mungos::Indexed>::indexes();
+            for nested in nested {
+                indexes.push(format!("{}.{}", stringify!(#ident), nested));
+            }
+        });
+        unique_indexes.push(quote! {
+            let nested = <#ty as mungos::Indexed>::unique_indexes();
+            for nested in nested {
+                unique_indexes.push(format!("{}.{}", stringify!(#ident), nested));
+            }
+        });
     }
 
     quote! {
-        impl #ident {
-            pub async fn collection(mungos: &mungos::Mungos, db: &str, create_index: bool)
-                -> anyhow::Result<mungos::Collection<#ident>>
-            {
-                let coll = mungos.collection(db, stringify!(#ident));
-
-                if create_index {
-                    #(coll.create_unique_index_from_doc(#unique_doc_indexes).await?;)*
-                    #(coll.create_index_from_doc(#doc_indexes).await?;)*
-                    #(coll.create_unique_index(stringify!(#unique_indexes)).await?;)*
-                    #(coll.create_index(stringify!(#indexes)).await?;)*
-                }
-
-                Ok(coll)
+        impl mungos::Indexed for #ident {
+            fn name() -> &'static str {
+                stringify!(#ident)
+            }
+            fn indexes() -> Vec<String> {
+                let mut indexes = Vec::new();
+                #(#indexes)*
+                indexes
+            }
+            fn unique_indexes() -> Vec<String> {
+                let mut unique_indexes = Vec::new();
+                #(#unique_indexes)*
+                unique_indexes
+            }
+            fn doc_indexes() -> Vec<mungos::mongodb::bson::Document> {
+                vec![#(#doc_indexes,)*]
+            }
+            fn unique_doc_indexes() -> Vec<mungos::mongodb::bson::Document> {
+                vec![#(#unique_doc_indexes,)*]
+            }
+        }
+        impl mungos::Indexed for Option<#ident> {
+            fn name() -> &'static str {
+                stringify!(#ident)
+            }
+            fn indexes() -> Vec<String> {
+                let mut indexes = Vec::new();
+                #(#indexes)*
+                indexes
+            }
+            fn unique_indexes() -> Vec<String> {
+                let mut unique_indexes = Vec::new();
+                #(#unique_indexes)*
+                unique_indexes
+            }
+            fn doc_indexes() -> Vec<mungos::mongodb::bson::Document> {
+                vec![#(#doc_indexes,)*]
+            }
+            fn unique_doc_indexes() -> Vec<mungos::mongodb::bson::Document> {
+                vec![#(#unique_doc_indexes,)*]
             }
         }
     }
